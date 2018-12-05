4b51bb507452b9412c22d8f42f02790a22eb7f05
nik31096 <nikkostin87@gmail.com>
2018-12-06 01:23:08 +0300

    Initial commit

--- assignment3.py
+++ assignment3.py
@@ -6,5 +6,37 @@ def softmax(x):
     exps = np.exp(shiftx)
     return exps / np.sum(exps)
 
+print("[INFO] glove vector downloading")
+with open('../../datasets/glove/glove.6B.50d.txt', 'r') as f:
+    data = f.read().splitlines()
+
+glove_dict = {}
+for vector in data:
+    tokens = vector.split(' ')
+    vector =  np.array([float(item) for item in tokens[1:]])
+    glove_dict[tokens[0]] = vector / np.linalg.norm(vector)
+
+a = 0
+for word, vector in glove_dict.items():
+    for word_, vector_ in glove_dict.items():
+        if word != word_:
+            a = np.dot(vector, vector_)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
